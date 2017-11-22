f2f5e7bfc2f25fd757293b9ff0de66955572de21
Igor Melo <igor.leite@genialinvestimentos.com.br>
2017-11-22 12:02:33 -0200

    FormControls

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,4 +1,8 @@
 export class User {
     email: string;
     password: string;
+    constructor(email?, password?) {
+        this.email = email;
+        this.password = password;
+    }
 }
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,3 +1,4 @@
+import { User } from './../../models/user';
 import { HomePage } from './../home/home';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, Platform, ToastController, LoadingController } from 'ionic-angular';
@@ -5,6 +6,7 @@ import * as firebase from 'firebase/app';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook';
 import { User } from '../../models/user';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 /**
  * Generated class for the LoginPage page.
  *
@@ -18,6 +20,7 @@ import { User } from '../../models/user';
   templateUrl: 'login.html',
 })
 export class LoginPage {
+  loginForm: FormGroup;
   response: boolean;
   user = {} as User;
   email: any;
@@ -26,7 +29,9 @@ export class LoginPage {
   constructor(public navCtrl: NavController,
     public navParams: NavParams, public afAuth: AngularFireAuth,
     public facebook: Facebook, public platform: Platform, private toastCtrl: ToastController,
-    public loadingCtrl: LoadingController) {
+    public fb: FormBuilder) {
+    this.initForm(fb);
+    this.user = new User();
   }
   async login(user: User) {
     this.response = true;
@@ -56,6 +61,30 @@ export class LoginPage {
   }
   cadastrar() {
     this.navCtrl.push('RegisterPage');
+  }
+
+  initForm(fb: FormBuilder) {
+    this.loginForm = fb.group({
+      email: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)
+        ])
+      ],
+      password: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(/[0-9]{6}/g)
+        ])
+      ]
+    })
+  }
+  submitForm(value: any) {
+    if (!this.loginForm.valid) return;
+    this.user = new User(value.email, value.password);
+    this.login(value);
   }
 
 
